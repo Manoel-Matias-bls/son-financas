0ff92b72139e2253c3030378fdb578e941957449
Manoel-Matias-bls <manoelmatias.cardoso@gmail.com>
2017-05-10 11:58:31 -0300

    .

---
+++ src/Application.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SONFin;
+#Classe para controle da aplicação
+
+class Application
+{
+    private $serviceContainer;
+
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service($name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service)
+    {
+        if (is_callable($service))
+        {
+            $this->serviceContainer->addLazy($name, $service);
+        }else
+            {
+            $this->serviceContainer->add($name,$service);
+        }
+    }
+
+}
